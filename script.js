6604516cc60b0602246d0bdd20fa1db543af969b
Daz <dalazdra@yahoo.fr>
2024-11-14 11:28:54 +0100

    test console OK

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 // ℹ️ La propriété console.log() affiche un message dans la console Web.
 // ℹ️ Elle prend en paramètre une string (on verra plus tard ce que c'est) et l'affiche dans la console.
 // 🦁 Ta mission est d'afficher : Hello World!
+console.log("Hello World!");
+console.log("Sonia de novembre 2024");
+
+/* 
+console.log() est une methode statique quipermet d'afficher un msg dans la console web . ce msg peut etre notamment une chaine d caractere / 
+ou un objet ou des chaines de subsitition
+*/
+
+// liste des plugins VSCODE
+/*
+- eslint
+- quokka
+- prettier
+*/
+
+console.error("sonia");
+console.info("sonia info");
+console.warn("sonia warn");

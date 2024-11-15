b2830da29b0c0d0e448a10e99fd3f0f4fef4adf4
Daz <dalazdra@yahoo.fr>
2024-11-15 16:59:57 +0100

    string OK

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // 🦁 Importe cette fonction
-// import { prompt } from "./helper.js";
+import { prompt } from "./helper.js";
 
 console.log("NODE-MAILER ™️");
 
 // 🦁 Récupère le mail, l'objet et le message
+const userEmail = await prompt("Entrez votre email : ");
+const objetEmail = await prompt("Entrez l'objet de votre email : ");
+if (userEmail.length === 0 || objetEmail.length === 0) {
+  console.info(" Vous n'avez pas renseigné de mail ou  d'objet ");
+  process.exit(1);
+}
 
+const msgEmail = await prompt("Entrez votre message : ");
 // 🦁 Affiche un log
-// - Indiquant que le mail est en train d'être envoyé
-// - Le mail du destinataire
-// - L'objet du mail
+console.log(`
+ 
+Le mail est en train d'être envoyé ..
+
+A : ${userEmail}
+Objet : ${objetEmail} 
+${msgEmail}`);
 // - Le message du mail
